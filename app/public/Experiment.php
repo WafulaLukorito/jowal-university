d299f582cfa144a8094ffee8e833ea856bb54acf
Lukorito <jonesdeelder@gmail.com>
2021-03-12 01:11:36 +0300

    Experiment.php

---
+++ app/public/Experiment.php
@@ -0,0 +1,17 @@
+
+<?php
+
+echo 2+2;
+$name = "Wafula Stock!";
+
+?>
+<h1>This page is all about <?= $name?> </h1>
+
+<?php 
+echo 5*5;
+
+
+echo "\n";
+echo " \n My name is ".$name;
+?>
+<h2>All about <?= $name ?></h2>
